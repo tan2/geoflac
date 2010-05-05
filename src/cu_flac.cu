80331191b9e88365065f4d72280963615aca0765
Eh Tan <tan2@mail.utexas.edu>
2010-05-05 15:09:32 -0500

    Passing the array size as function argument, not as template argument

--- src/cu_flac.cu
+++ src/cu_flac.cu
@@ -543,7 +543,7 @@ void cu_flac(double *force, double *balance, double *vel,
     cudaStreamSynchronize(stream1);
     cudaMemcpy(vel, vel_d, nx*nz*2*sizeof(double),
                cudaMemcpyDeviceToHost);
-    *boff = reduction<double,nx*nz*ndim,MAX>(balance_d);
+    *boff = reduction<double,MAX>(balance_d, nx*nz*ndim, stream1);
 
     cudaMemcpyAsync(force, force_d, nx*nz*2*sizeof(double),
                     cudaMemcpyDeviceToHost, stream1);
