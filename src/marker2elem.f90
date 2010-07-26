6f9c6de1d9e48c0060f6edf7a683980ea5e528c7
Eh Tan <tan2@mail.utexas.edu>
2010-07-26 16:18:08 -0500

    Simplify marker phase calculation

--- src/marker2elem.f90
+++ src/marker2elem.f90
@@ -9,55 +9,31 @@ subroutine marker2elem
   parameter( min_elmarkers = 0, max_elmarkers = 12 )
   !type(marker):: mark(nmarkers)
 
-  kkk = nmarkers
-  xiph = 0.
-  ! Interpolate marker properties into elements 
+  ! Interpolate marker properties into elements
   ! Find the triangle in which each marker belongs
-  !old_aps = aps
-  iphase_old = iphase
-  ophase_ratio = phase_ratio
-  !aps = 0.
-  iphase = 0.
-  phase_ratio = 0.
+
+
   do j = 1 , nz-1
       do i = 1 , nx-1
-          kinc = 0
-          meml= 0
-          phase_counter = 0.0
-          do k = 1 , 2
-              do l = 1 , kkk 
-                  if(mark(l)%dead.eq.0) cycle
-                  n = 2*( (nz-1)*(i-1) +j-1) + k 
-                  ntra = mark(l)%ntriag
-                  if (n.ne.ntra) then
-                      cycle 
-                  else
-                      kinc = kinc + 1
-
-                      iphase(j,i) = iphase(j,i)+ mark(l)%phase
-                      meml = l
-                      ! To linearly interpolate for density, conductivities, viscosities calculate phases ratios
-                      do m = 1,nphasl
-                          if(mark(l)%phase.eq.lphase(m)) phase_counter(m) = phase_counter(m) + 1
-                      enddo
-                  endif
-              enddo ! m
-          enddo ! k
+          kinc = sum(nphase_counter(j,i,1:nphasl))
 
           !  if there are too few markers in the elmenent, create a new one
-          if(kinc.le.2) then
-              nmarkers = nmarkers+1
-              kinc = kinc +1
+          do while (kinc.le.2)
+              call SysMsg('marker2elem: too few markers in the elmenent, create a new one')
+              nmarkers = nmarkers + 1
+              kinc = kinc + 1
               x1 = cord(j  ,i+1,1)
               x2 = cord(j+1,i  ,1)
               x3 = cord(j+1,i+1,1)
               y1 = cord(j  ,i+1,2)
               y2 = cord(j+1,i  ,2)
               y3 = cord(j+1,i+1,2)
-              xx = x2 +0.5*(x3-x2)  
+
+              xx = x2 + 0.5*(x3-x2)
               mark(nmarkers)%x = xx
               yy = y1 +0.5*(y3-y1)  
               mark(nmarkers)%y = yy
+
               ! Calculate barycentic coordinates
               ii = i
               jj = j
@@ -76,36 +52,11 @@ subroutine marker2elem
               mark(nmarkers)%mtemp = tmpr 
               mark(nmarkers)%ID = nmarkers 
               ! assign phase to the new marker
-              if(kinc.gt.1) then
-                  mark(nmarkers)%phase = mark(meml)%phase 
-                  iphase(j,i) = iphase(j,i)+mark(nmarkers)%phase
-                  !           write(*,*) i,j,xiph,n,ntr,xiph,kinc,mark(nmarkers)%phase
-                  ! if there were no more markers in the new element
-              else 
-                  !          xiph = 0.20*(iphase_old(j,i)+iphase_old(j,i-1)+iphase_old(j,i+1)+iphase_old(j-1,i)+iphase_old(j+1,i))
-                  xiph = iphase_old(j,i)
-                  !	if (i.eq.1) xiph = (iphase_old(j-1,i)+iphase_old(j+1,i))/2.
-                  !	if (i.eq.nx-1) xiph = (iphase_old(j-1,i)+iphase_old(j+1,i))/2.
-                  !	if (j.eq.1) xiph = (iphase_old(j,i+1)+iphase_old(j,i-1))/2.
-                  !	if (j.eq.nz-1) xiph = (iphase_old(j,i-1)+iphase_old(j,i+1))/2.
-                  !	if (j.eq.1.and.i.eq.1) xiph = (iphase_old(j,i+1)+iphase_old(j+1,i)+iphase_old(j+1,i+1))/3.
-                  !	if (j.eq.nz-1.and.i.eq.nx-1) xiph = (iphase_old(j,i-1)+iphase_old(j-1,i)+iphase_old(j-1,i-1))/3.
-                  !	if (j.eq.1.and.i.eq.nx-1) xiph = (iphase_old(j,i-1)+iphase_old(j+1,i)+iphase_old(j+1,i-1))/3.
-                  !	if (j.eq.nz-1.and.i.eq.1) xiph = (iphase_old(j,i+1)+iphase_old(j-1,i)+iphase_old(j-1,i+1))/3.
-                  !	if (i.eq.1) xiph = iphase_old(j,5)
-                  mark(nmarkers)%phase = iphase(j,i)
-                  iphase(j,i) = iphase(j,i)+mark(nmarkers)%phase
-                  !write(*,*) xiph,iphase_old(j,i-1),iphase_old(j,i+1),iphase_old(j-1,i),iphase_old(j+1,i)
-                  !write(333,*) xiph,iphase_old(j,i-1),iphase_old(j,i+1),iphase_old(j-1,i),iphase_old(j+1,i)
-              endif
-              do m = 1,nphasl
-                  if(mark(nmarkers)%phase.eq.lphase(m)) phase_counter(m) = phase_counter(m) + 1
-              enddo
-          endif
-          do kn = 1 , nphasl
-              phase_ratio (j,i,kn) = phase_counter(kn)/kinc     
+              mark(nmarkers)%phase = iphase(j,i)
+              nphase_counter(j,i,mark(nmarkers)%phase) = nphase_counter(j,i,mark(nmarkers)%phase) + 1
           enddo
-          iphase(j,i) = iphase(j,i)/kinc
+
+          phase_ratio (j,i,1:nphasl) = nphase_counter(j,i,1:nphasl) / float(kinc)
 
       enddo
   enddo
