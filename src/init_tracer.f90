69d0fdd9641defd486bee92ddce35f66e38be96d
Eh Tan <tan2@mail.utexas.edu>
2010-08-25 16:43:54 -0500

    Fixed a bug in init_tracer. Reset values of dead tracers. Except the order of tracers are different, this version give identical tracer output as previous tracer output.

--- src/init_tracer.f90
+++ src/init_tracer.f90
@@ -2,11 +2,12 @@ subroutine init_tracer
 USE marker_data
 
 include 'precision.inc'
+include 'params.inc'
 include 'arrays.inc'
 
 common /tracers/ idtracer(2*mnz*mnx)
 
-dimension ielem(2,mnz,mnx)
+dimension ielem(mnz,mnx)
 
 ielem = 0
 nmtracers = 0
@@ -18,17 +19,17 @@ do kk = 1,nmarkers
     j = mod(nn, nz-1) + 1
     i = nn/(nz-1) + 1
 
-    if(ielem(k,j,i) == 0) then
+    if(ielem(j,i) == 0) then
         do n = 1, nzone_tracer
             if(i >= itx1(n) .and. i <= itx2(n) .and. &
                  j >= ity1(n) .and. j <= ity2(n)) then
-                ielem(k,j,i) = 1
+                ielem(j,i) = 1
                 nmtracers = nmtracers + 1
                 ! Storing the array index of this marker, assuming that
                 ! markers never move in the array
                 idtracer(nmtracers) = kk
             endif
-          enddo
+        enddo
     endif
 enddo
 return
