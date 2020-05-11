06a10524137912c989ffd9aad40bd7006c6f4300
Eh Tan <tan2@earth.sinica.edu.tw>
2020-05-11 18:12:34 +0800

    removed unused subroutine

--- src/bar2euler.f90
+++ src/bar2euler.f90
@@ -78,44 +78,6 @@ subroutine shape_functions(j, i, shp2)
 end subroutine shape_functions
 
 
-subroutine shape_functions1(j, i, k, shp)
-  use arrays
-  use params
-  implicit none
-  integer :: j, i, k
-  double precision, intent(out) :: shp(2,3)
-  double precision :: x1, x2, x3, y1, y2, y3, det
-
-  if (k.eq.1) then
-     x1 = cord(j  ,i  ,1)
-     x2 = cord(j+1,i  ,1)
-     x3 = cord(j  ,i+1,1)
-     y1 = cord(j  ,i  ,2)
-     y2 = cord(j+1,i  ,2)
-     y3 = cord(j  ,i+1,2)
-  else  !if (k.eq.2) then
-     x1 = cord(j  ,i+1,1)
-     x2 = cord(j+1,i  ,1)
-     x3 = cord(j+1,i+1,1)
-     y1 = cord(j  ,i+1,2)
-     y2 = cord(j+1,i  ,2)
-     y3 = cord(j+1,i+1,2)
-  endif
-
-  ! Calculate triangle properties
-  det=( (x2*y3-y2*x3) - (x1*y3-y1*x3) + (x1*y2-y1*x2) )
-
-  !Find the parameters ONLY for 2 vertices
-  shp(1,1) = (x2*y3-y2*x3)/det
-  shp(1,2) = (y2-y3)/det
-  shp(1,3) = (x3-x2)/det
-  shp(2,1) = (x3*y1-y3*x1)/det
-  shp(2,2) = (y3-y1)/det
-  shp(2,3) = (x1-x3)/det
-
-end subroutine shape_functions1
-
-
 ! For brevity shp(1,2) --> s12 etc
 !
 ! a1 = s11 + s12*x + s13*y
