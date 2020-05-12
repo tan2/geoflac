fc9902df79c8d21d90e9c30c231c957085d69f2a
Eh Tan <tan2@earth.sinica.edu.tw>
2020-05-12 10:51:10 +0800

    added myrandom(), thread-safe prng

---
+++ src/myrandom_mod.f90
@@ -0,0 +1,20 @@
+module myrandom_mod
+
+contains 
+
+!
+! A simple thread-safe pseudo random number generator
+!
+
+subroutine myrandom(iseed, r)
+    integer, intent(inout) :: iseed
+    double precision, intent(out) :: r
+
+    iseed = mod(8121 * iseed + 28411, 134456 )
+    r = (iseed * 1.0d0) / 134456
+
+    return
+end subroutine myrandom
+
+
+end module myrandom_mod
