2259f281312e127e2c323215cf60a6ee44e6e356
Eh Tan <tan2@earth.sinica.edu.tw>
2020-05-12 16:13:03 +0800

    Add ACC directives

--- src/lpeuler2bar.f90
+++ src/lpeuler2bar.f90
@@ -9,9 +9,11 @@ integer :: n, k, i, j, ntr, inc
 double precision :: xx, yy, bar1, bar2
 character*200 msg
 
+!$ACC parallel
 nphase_counter(:,:,:) = 0
 ntopmarker(:) = 0
 
+!$ACC loop
 !$OMP parallel do private(n,k,j,i,xx,yy,bar1,bar2,ntr,inc)
 do n = 1 , nmarkers
     if (mark_dead(n).eq.0) cycle
@@ -35,24 +37,28 @@ do n = 1 , nmarkers
     mark_ntriag(n) = ntr
 
     !$OMP critical (lpeulerbar1)
+    !$ACC atomic update
     nphase_counter(mark_phase(n),j,i) = nphase_counter(mark_phase(n),j,i) + 1
     !$OMP end critical (lpeulerbar1)
 
     if(j == 1) then
         if(ntopmarker(i) == max_markers_per_elem) then
-            write(msg,*) 'Too many markers at surface elements in lpeuler2bar:', i, n
-            call SysMsg(msg)
+            !write(msg,*) 'Too many markers at surface elements in lpeuler2bar:', i, n
+            !call SysMsg(msg)
             mark_dead(n) = 0
             cycle
         endif
         !$OMP critical (lpeulerbar2)
         ! recording the id of markers belonging to surface elements
+        !$ACC atomic update
         ntopmarker(i) = ntopmarker(i) + 1
+        !$ACC atomic write
         itopmarker(ntopmarker(i), i) = n
         !$OMP end critical (lpeulerbar2)
     end if
 enddo
 !$OMP end parallel do
+!$ACC end parallel
 return
 
 end subroutine lpeuler2bar
