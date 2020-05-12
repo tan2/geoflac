63eebe5aa9824d5fc36ac6c02f2b83bafafa9b03
Eh Tan <tan2@earth.sinica.edu.tw>
2020-05-12 10:51:10 +0800

    added myrandom()

---
+++ src/myrandom.f90
@@ -0,0 +1,21 @@
+module myrandom
+
+contains 
+
+!
+! A simple thread-safe pseudo random number generator
+!
+
+subroutine myrandom(iseed, r)
+    !$ACC routine seq
+    integer, intent(inout) :: iseed
+    double precision, intent(out) :: r
+
+    iseed = mod(8121 * iseed + 28411, 134456 )
+    r = (iseed * 1.0d0) / 134456
+
+    return
+end subroutine myrandom
+
+
+end module myrandom
