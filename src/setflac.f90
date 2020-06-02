2f7e34a43e9b6af2d5a3e2797b76a146e18a8d84
Eh Tan <tan2@earth.sinica.edu.tw>
2020-06-02 15:37:11 +0800

    Adjust sequence in setflac.f90

--- src/setflac.f90
+++ src/setflac.f90
@@ -44,14 +44,27 @@ call init_stress
 ! Setup boundary conditions
 call init_bc
 
+!Initialization
+!$ACC update device(temp, vel, stress0, force, balance, amass, rmass, &
+!$ACC               area, dvol, strain, bc, ncod, junk2, xmpt, tkappa, &
+!$ACC               iphase, nphase_counter, ntopmarker, itopmarker, irheol_fl, &
+!$ACC               nopbou, ncodbou, idtracer, phase_ratio, dtopo, dhacc, extrusion, &
+!$ACC               andesitic_melt_vol, extr_acc, strainr, aps, visn, e2sr, &
+!$ACC               temp0, source, shrheat, bcstress, &
+!$ACC               pt, barcord, cold, cnew, numtr, &
+!$ACC               se2sr, sshrheat)
+
 ! Distribution of REAL masses to nodes
 call rmasses
 
 ! Initialization of viscosity
+!$ACC update self(irheol,visn,cord)
 if( ivis_present.eq.1 ) call init_visc
+!$ACC update device(visn)
 
 ! Inertial masses and time steps (elastic and maxwell)
 call dt_mass
+
 !$ACC update self(dt_elastic, dt_maxwell)
 dt = min( dt_elastic, dt_maxwell )
 !$ACC update device(dt)
@@ -61,14 +74,7 @@ dtavg=0
 nsrate=-1
 
 !Initialization
-!$ACC update device(temp, vel, stress0, force, balance, amass, rmass, &
-!$ACC               area, dvol, strain, bc, ncod, junk2, xmpt, tkappa, &
-!$ACC               iphase, nphase_counter, ntopmarker, itopmarker, irheol_fl, &
-!$ACC               nopbou, ncodbou, idtracer, phase_ratio, dtopo, dhacc, extrusion, &
-!$ACC               andesitic_melt_vol, extr_acc, strainr, aps, visn, e2sr, &
-!$ACC               temp0, source, shrheat, bcstress, &
-!$ACC               pt, barcord, cold, cnew, numtr, &
-!$ACC               se2sr, sshrheat, dtavg, nsrate)
+!$ACC update device(dtavg, nsrate)
 
 
 return
