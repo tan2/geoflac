3e851b0a1d20b9241ca78539d7232045a163110f
Eh Tan <tan2@earth.sinica.edu.tw>
2020-08-18 13:49:13 +0800

    Reindent

--- src/dt_mass.f90
+++ src/dt_mass.f90
@@ -66,80 +66,80 @@ end if
 
 do iblk = 1, 2
     do jblk = 1, 2
-!$OMP parallel do private(iph, pwave, dens, vel_sound, rho_inert, rho_inert2, &
-!$OMP                     am3, dte, diff, dtt, dt_m, rmu)
-do i = iblk, nx-1, 2
-    do j = jblk, nz-1, 2
+        !$OMP parallel do private(iph, pwave, dens, vel_sound, rho_inert, rho_inert2, &
+        !$OMP                     am3, dte, diff, dtt, dt_m, rmu)
+        do i = iblk, nx-1, 2
+            do j = jblk, nz-1, 2
 
-        iph     = iphase(j,i)
-        pwave   = rl(iph) + 0.6666*rm(iph)
-        dens    = den(iph)
-        vel_sound = dlmin*frac/dt_elastic
-        rho_inert = pwave/(vel_sound*vel_sound)
-        if (i_rey.eq.1.and.vel_max.gt.0.) then
-            rho_inert2 = (xReyn*v_min)/(vel_max*abs(rzbo))
-!           write(*,*) rho_inert, rho_inert2,vel_max
-            if (rho_inert.gt.rho_inert2) rho_inert = rho_inert2
-        endif
-        ! Find the inert. density for given geometry, elas_mod and dt_scale
-        ! idt_scale = 0 (dt = frac*dx_min * sqrt(dens/pwave) )
-        ! idt_scale = 1 (dt is taken from sup.dat: dt = dt_scale)
-        ! idt_scale = 2 (dt = frac*dx_min * tolerance/Vbc_max)
-        if (idt_scale.gt.0) then
-
-            ! Distribution 1/3 of the inertial mass of each element to the nodes
-            ! am3=c1d12*area(j,i,ii)*pwave*(dlmax*dt_scale/frac)**2
-            ! 1/12 = 1/3 * 1/2 (2 meshes) * 1/2 (1/area_num = 2 area_real)
-            am3=c1d12*rho_inert/area(j,i,1)
-            amass(j  ,i  ) = amass(j  ,i  ) + am3
-            amass(j+1,i  ) = amass(j+1,i  ) + am3
-            amass(j  ,i+1) = amass(j  ,i+1) + am3
-
-            am3=c1d12*rho_inert/area(j,i,2)
-            amass(j+1,i  ) = amass(j+1,i  ) + am3
-            amass(j+1,i+1) = amass(j+1,i+1) + am3
-            amass(j  ,i+1) = amass(j  ,i+1) + am3
-
-            am3=c1d12*rho_inert/area(j,i,3)
-            amass(j  ,i  ) = amass(j  ,i  ) + am3
-            amass(j+1,i  ) = amass(j+1,i  ) + am3
-            amass(j+1,i+1) = amass(j+1,i+1) + am3
-
-            am3=c1d12*rho_inert/area(j,i,4)
-            amass(j  ,i  ) = amass(j  ,i  ) + am3
-            amass(j+1,i+1) = amass(j+1,i+1) + am3
-            amass(j  ,i+1) = amass(j  ,i+1) + am3
-
-        else  ! idt_scale=0
-            ! Find the dtime for given geometry, density and elas_mod
-            dte = frac*dlmin*sqrt(dens/pwave)
-            dt_elastic = min(dt_elastic,dte)
-        endif
-
-        ! Find the maximum THERMAL time step from Stability Criterion
-        ! dtmax = dxmin^2/diffusivity = dx^2/(lyamda/cp*dens)
-        diff = Eff_conduct(j,i)/den(iph)/Eff_cp(j,i)
-        dtt = dlmin*dlmin/diff
-        dtmax_therm =min (dtmax_therm,dtt)
-
-        ! Calculate maxwell time step
-        if (ivis_present .eq. 1) then
-            !dt_m =visn(j,i)/rm(iph)*fracm
-            if( (irheol(iph).eq.3 .OR. irheol(iph).eq.12) .AND. rm(iph).lt.1.e+11 ) then
-                visc_cut = 1.e+10
-                if( v_min .lt. visc_cut ) then
-                    rmu = rm(iph) * v_min/visc_cut
-                else
-                    rmu = rm(iph)
+                iph     = iphase(j,i)
+                pwave   = rl(iph) + 0.6666*rm(iph)
+                dens    = den(iph)
+                vel_sound = dlmin*frac/dt_elastic
+                rho_inert = pwave/(vel_sound*vel_sound)
+                if (i_rey.eq.1.and.vel_max.gt.0.) then
+                    rho_inert2 = (xReyn*v_min)/(vel_max*abs(rzbo))
+        !           write(*,*) rho_inert, rho_inert2,vel_max
+                    if (rho_inert.gt.rho_inert2) rho_inert = rho_inert2
                 endif
-                dt_m =v_min/rmu * fracm
-                dt_maxwell = min (dt_m,dt_maxwell)
-            endif
-        endif
+                ! Find the inert. density for given geometry, elas_mod and dt_scale
+                ! idt_scale = 0 (dt = frac*dx_min * sqrt(dens/pwave) )
+                ! idt_scale = 1 (dt is taken from sup.dat: dt = dt_scale)
+                ! idt_scale = 2 (dt = frac*dx_min * tolerance/Vbc_max)
+                if (idt_scale.gt.0) then
 
-    enddo
-enddo
-!$OMP end parallel do
+                    ! Distribution 1/3 of the inertial mass of each element to the nodes
+                    ! am3=c1d12*area(j,i,ii)*pwave*(dlmax*dt_scale/frac)**2
+                    ! 1/12 = 1/3 * 1/2 (2 meshes) * 1/2 (1/area_num = 2 area_real)
+                    am3=c1d12*rho_inert/area(j,i,1)
+                    amass(j  ,i  ) = amass(j  ,i  ) + am3
+                    amass(j+1,i  ) = amass(j+1,i  ) + am3
+                    amass(j  ,i+1) = amass(j  ,i+1) + am3
+
+                    am3=c1d12*rho_inert/area(j,i,2)
+                    amass(j+1,i  ) = amass(j+1,i  ) + am3
+                    amass(j+1,i+1) = amass(j+1,i+1) + am3
+                    amass(j  ,i+1) = amass(j  ,i+1) + am3
+
+                    am3=c1d12*rho_inert/area(j,i,3)
+                    amass(j  ,i  ) = amass(j  ,i  ) + am3
+                    amass(j+1,i  ) = amass(j+1,i  ) + am3
+                    amass(j+1,i+1) = amass(j+1,i+1) + am3
+
+                    am3=c1d12*rho_inert/area(j,i,4)
+                    amass(j  ,i  ) = amass(j  ,i  ) + am3
+                    amass(j+1,i+1) = amass(j+1,i+1) + am3
+                    amass(j  ,i+1) = amass(j  ,i+1) + am3
+
+                else  ! idt_scale=0
+                    ! Find the dtime for given geometry, density and elas_mod
+                    dte = frac*dlmin*sqrt(dens/pwave)
+                    dt_elastic = min(dt_elastic,dte)
+                endif
+
+                ! Find the maximum THERMAL time step from Stability Criterion
+                ! dtmax = dxmin^2/diffusivity = dx^2/(lyamda/cp*dens)
+                diff = Eff_conduct(j,i)/den(iph)/Eff_cp(j,i)
+                dtt = dlmin*dlmin/diff
+                dtmax_therm =min (dtmax_therm,dtt)
+
+                ! Calculate maxwell time step
+                if (ivis_present .eq. 1) then
+                    !dt_m =visn(j,i)/rm(iph)*fracm
+                    if( (irheol(iph).eq.3 .OR. irheol(iph).eq.12) .AND. rm(iph).lt.1.e+11 ) then
+                        visc_cut = 1.e+10
+                        if( v_min .lt. visc_cut ) then
+                            rmu = rm(iph) * v_min/visc_cut
+                        else
+                            rmu = rm(iph)
+                        endif
+                        dt_m =v_min/rmu * fracm
+                        dt_maxwell = min (dt_m,dt_maxwell)
+                    endif
+                endif
+
+            enddo
+        enddo
+        !$OMP end parallel do
 
     enddo
 enddo
