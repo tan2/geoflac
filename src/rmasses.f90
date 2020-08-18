33f2b9b574b5d622e8b0e6f385d4113a055b9abf
Eh Tan <tan2@earth.sinica.edu.tw>
2020-08-18 13:43:12 +0800

    Reindent

--- src/rmasses.f90
+++ src/rmasses.f90
@@ -21,38 +21,37 @@ rmass = 0
 
 do iblk = 1, 2
     do jblk = 1, 2
+        !$OMP parallel do private(dens)
+        do i = iblk, nx-1, 2
+            do j = jblk, nz-1, 2
 
-!$OMP parallel do private(dens)
-do i = iblk, nx-1, 2
-    do j = jblk, nz-1, 2
+                !  Area and densities of zones
+                dens = Eff_dens( j,i )
 
-        !  Area and densities of zones
-        dens = Eff_dens( j,i )
+                ! Distribution 1/3 of the mass of each element to the nodes
+                ! *0.5 - becuase 1/area is double of real area; *0.5 - 2 grids
 
-        ! Distribution 1/3 of the mass of each element to the nodes 
-        ! *0.5 - becuase 1/area is double of real area; *0.5 - 2 grids
+                ! (1) Element A:
+                rmass(j  ,i  )=rmass(j  ,i  )+c1d12/area(j,i,1)*dens
+                rmass(j+1,i  )=rmass(j+1,i  )+c1d12/area(j,i,1)*dens
+                rmass(j  ,i+1)=rmass(j  ,i+1)+c1d12/area(j,i,1)*dens
+                ! (2) Element B:
+                rmass(j+1,i+1)=rmass(j+1,i+1)+c1d12/area(j,i,2)*dens
+                rmass(j+1,i  )=rmass(j+1,i  )+c1d12/area(j,i,2)*dens
+                rmass(j  ,i+1)=rmass(j  ,i+1)+c1d12/area(j,i,2)*dens
 
-        ! (1) Element A:
-        rmass(j  ,i  )=rmass(j  ,i  )+c1d12/area(j,i,1)*dens
-        rmass(j+1,i  )=rmass(j+1,i  )+c1d12/area(j,i,1)*dens 
-        rmass(j  ,i+1)=rmass(j  ,i+1)+c1d12/area(j,i,1)*dens 
-        ! (2) Element B:
-        rmass(j+1,i+1)=rmass(j+1,i+1)+c1d12/area(j,i,2)*dens 
-        rmass(j+1,i  )=rmass(j+1,i  )+c1d12/area(j,i,2)*dens 
-        rmass(j  ,i+1)=rmass(j  ,i+1)+c1d12/area(j,i,2)*dens 
+                ! (3) Element C:
+                rmass(j  ,i  )=rmass(j  ,i  )+c1d12/area(j,i,3)*dens
+                rmass(j+1,i  )=rmass(j+1,i  )+c1d12/area(j,i,3)*dens
+                rmass(j+1,i+1)=rmass(j+1,i+1)+c1d12/area(j,i,3)*dens
 
-        ! (3) Element C:
-        rmass(j  ,i  )=rmass(j  ,i  )+c1d12/area(j,i,3)*dens
-        rmass(j+1,i  )=rmass(j+1,i  )+c1d12/area(j,i,3)*dens 
-        rmass(j+1,i+1)=rmass(j+1,i+1)+c1d12/area(j,i,3)*dens 
-
-        ! (4) Element D:
-        rmass(j  ,i  )=rmass(j  ,i  )+c1d12/area(j,i,4)*dens
-        rmass(j+1,i+1)=rmass(j+1,i+1)+c1d12/area(j,i,4)*dens 
-        rmass(j  ,i+1)=rmass(j  ,i+1)+c1d12/area(j,i,4)*dens 
-    enddo
-enddo
-!$OMP end parallel do
+                ! (4) Element D:
+                rmass(j  ,i  )=rmass(j  ,i  )+c1d12/area(j,i,4)*dens
+                rmass(j+1,i+1)=rmass(j+1,i+1)+c1d12/area(j,i,4)*dens
+                rmass(j  ,i+1)=rmass(j  ,i+1)+c1d12/area(j,i,4)*dens
+            enddo
+        enddo
+        !$OMP end parallel do
     enddo
 enddo
 
