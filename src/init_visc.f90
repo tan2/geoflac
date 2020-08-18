80ab7be6abca516e21d67e15a39faf77241a242d
Eh Tan <tan2@earth.sinica.edu.tw>
2020-08-18 14:09:29 +0800

    Parallelize init_visc()

--- src/init_visc.f90
+++ src/init_visc.f90
@@ -4,10 +4,12 @@ subroutine init_visc
   use params
   include 'precision.inc'
 
-  do i = 1,nx-1  
+  !$OMP parallel do
+  do i = 1,nx-1
       do j = 1,nz-1
           visn(j,i) = Eff_visc(j,i)
       end do
   end do
+  !$OMP end parallel do
   return
 end subroutine init_visc
