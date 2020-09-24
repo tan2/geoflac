b73f0dec3fcb5ddee900ca96cf07bb438e3df77a
Eh Tan <tan2@earth.sinica.edu.tw>
2020-09-24 16:38:16 +0800

    Add test case and script to reproducible runs

---
+++ benchmark/compare.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3
+from __future__ import print_function
+import sys, os
+import numpy as np
+import flac
+
+class Stuff():
+    pass
+
+
+def read_data(fl, frame):
+    stuff = Stuff()
+    stuff.T = fl.read_temperature(frame)
+    stuff.x, stuff.z = fl.read_mesh(frame)
+    stuff.vx, stuff.vz = fl.read_vel(frame)
+
+    stuff.m_x, stuff.m_z, stuff.m_age, stuff.m_phase, stuff.m_ID = \
+        fl.read_markers(frame)
+    return stuff
+
+
+def reldiff(oldf, newf):
+    m = np.abs(oldf).max()
+    diff = np.abs(newf - oldf)
+    return diff.max()/m, diff.std()/m
+
+
+def compare(old, new):
+    max, sigma = reldiff(old.T, new.T)
+    print('  Temperature:\t', max, sigma)
+
+    max, sigma = reldiff(old.x, new.x)
+    print('  X coordinate:\t', max, sigma)
+
+    max, sigma = reldiff(old.z, new.z)
+    print('  Z coordinate:\t', max, sigma)
+
+    max, sigma = reldiff(old.vx, new.vx)
+    print('  X velocity:\t', max, sigma)
+
+    max, sigma = reldiff(old.vz, new.vz)
+    print('  Z velocity:\t', max, sigma)
+
+    max, sigma = reldiff(old.m_x, new.m_x)
+    print('  Marker X:\t', max, sigma)
+
+    max, sigma = reldiff(old.m_z, new.m_z)
+    print('  Marker Z:\t', max, sigma)
+
+    max, sigma = reldiff(old.m_phase, new.m_phase)
+    print('  Marker Phase:\t', max, sigma)
+
+    return
+
+
+frame = int(sys.argv[1])
+curdir = os.getcwd()
+olddir = 'orig/'
+newdir = './'
+
+# name holder
+old = 0
+new = 0
+
+try:
+    # read old and new results
+
+    os.chdir(olddir)
+    flo = flac.Flac()
+    old = read_data(flo, frame)
+
+    os.chdir('../' + newdir)
+    fln = flac.Flac()
+    new = read_data(fln, frame)
+
+    # compare results
+    print()
+    print('Relative difference (max, stddev) of frame =', frame,
+          ' step =', int(flo.steps[frame]))
+    compare(old, new)
+
+finally:
+    # restort to original directory
+    os.chdir(curdir)
