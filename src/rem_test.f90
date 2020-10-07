eba1786f25e86f16a14d2ec4349601213f77d70a
Eh Tan <tan2@earth.sinica.edu.tw>
2020-10-07 17:43:16 +0800

    Reindent

--- src/rem_test.f90
+++ src/rem_test.f90
@@ -37,61 +37,61 @@ subroutine itest_mesh(need_remeshing)
   jmint = 0
 
   if (need_remeshing == 0) then
-  do i = 1, nx-1
-      do j = 1,nz-1
-          ! loop for each 4 sub-triangles
-          do ii = 1,4
-              if (ii.eq.1) then
-                  iv(1) = i ; jv(1) = j ; iv(2) = i ; jv(2) = j+1 ; iv(3) = i+1 ; jv(3) = j
-              elseif (ii.eq.2) then
-                  iv(1) = i ; jv(1) = j+1 ; iv(2) = i+1 ; jv(2) = j+1 ; iv(3) = i+1 ; jv(3) = j
-              elseif (ii.eq.3) then
-                  iv(1) = i ; jv(1) = j ; iv(2) = i ; jv(2) = j+1 ; iv(3) = i+1 ; jv(3) = j+1
-              elseif (ii.eq.4) then
-                  iv(1) = i ; jv(1) = j ; iv(2) = i+1 ; jv(2) = j+1 ; iv(3) = i+1 ; jv(3) = j
-              endif
-              iv(4) = iv(1)
-              jv(4) = jv(1)
+    do i = 1, nx-1
+        do j = 1,nz-1
+            ! loop for each 4 sub-triangles
+            do ii = 1,4
+                if (ii.eq.1) then
+                    iv(1) = i ; jv(1) = j ; iv(2) = i ; jv(2) = j+1 ; iv(3) = i+1 ; jv(3) = j
+                elseif (ii.eq.2) then
+                    iv(1) = i ; jv(1) = j+1 ; iv(2) = i+1 ; jv(2) = j+1 ; iv(3) = i+1 ; jv(3) = j
+                elseif (ii.eq.3) then
+                    iv(1) = i ; jv(1) = j ; iv(2) = i ; jv(2) = j+1 ; iv(3) = i+1 ; jv(3) = j+1
+                elseif (ii.eq.4) then
+                    iv(1) = i ; jv(1) = j ; iv(2) = i+1 ; jv(2) = j+1 ; iv(3) = i+1 ; jv(3) = j
+                endif
+                iv(4) = iv(1)
+                jv(4) = jv(1)
 
-              ! Find all angles using vector dot product a*b = |a||b|cos(a)
-              do k = 2,3
-                  xa = cord(jv(k+1),iv(k+1),1)-cord(jv(k),iv(k),1)
-                  ya = cord(jv(k+1),iv(k+1),2)-cord(jv(k),iv(k),2)
-                  xxal = sqrt(xa*xa + ya*ya) 
-                  xb = cord(jv(k-1),iv(k-1),1)-cord(jv(k),iv(k),1)
-                  yb = cord(jv(k-1),iv(k-1),2)-cord(jv(k),iv(k),2)
-                  xxbl = sqrt(xb*xb + yb*yb) 
+                ! Find all angles using vector dot product a*b = |a||b|cos(a)
+                do k = 2,3
+                    xa = cord(jv(k+1),iv(k+1),1)-cord(jv(k),iv(k),1)
+                    ya = cord(jv(k+1),iv(k+1),2)-cord(jv(k),iv(k),2)
+                    xxal = sqrt(xa*xa + ya*ya) 
+                    xb = cord(jv(k-1),iv(k-1),1)-cord(jv(k),iv(k),1)
+                    yb = cord(jv(k-1),iv(k-1),2)-cord(jv(k),iv(k),2)
+                    xxbl = sqrt(xb*xb + yb*yb) 
 
-                  angle(k) = raddeg*acos((xa*xb+ya*yb)/(xxal*xxbl))
-              end do
-              angle (1) = 180.d0-angle(2)-angle(3)
+                    angle(k) = raddeg*acos((xa*xb+ya*yb)/(xxal*xxbl))
+                end do
+                angle (1) = 180.d0-angle(2)-angle(3)
 
-              ! min angle in one trianle
-              anglemin1 = min(angle(1),angle(2),angle(3))
+                ! min angle in one trianle
+                anglemin1 = min(angle(1),angle(2),angle(3))
 
-              ! min angle in the whole mesh
-              anglemint = min(anglemint, anglemin1)
+                ! min angle in the whole mesh
+                anglemint = min(anglemint, anglemin1)
 
-          end do
+            end do
 
-      end do
-  end do
+        end do
+    end do
 
-  if( dtout_screen .ne. 0 ) then
-      write (6,'(A,F6.2,A,F10.6)') '        min.angle=',anglemint, '     dt(yr)=',dt/sec_year
-      write (333,'(A,F6.2,A,F10.6)') '        min.angle=',anglemint, '     dt(yr)=',dt/sec_year
-      flush (333)
-  endif
-  ! check if the angle is smaller than angle of remeshing  
-  if (anglemint .le. angle_rem) then
-      if( dtout_screen .ne. 0 ) then
-          print *, 'Remeshing due to angle required.'
-          write(333,*) 'Remeshing due to angle required.'
-      else
-          call SysMsg('TEST_MESH: Remeshing due to angle required.')
-      endif
-      need_remeshing = 1
-  endif
+    if( dtout_screen .ne. 0 ) then
+        write (6,'(A,F6.2,A,F10.6)') '        min.angle=',anglemint, '     dt(yr)=',dt/sec_year
+        write (333,'(A,F6.2,A,F10.6)') '        min.angle=',anglemint, '     dt(yr)=',dt/sec_year
+        flush (333)
+    endif
+    ! check if the angle is smaller than angle of remeshing  
+    if (anglemint .le. angle_rem) then
+        if( dtout_screen .ne. 0 ) then
+            print *, 'Remeshing due to angle required.'
+            write(333,*) 'Remeshing due to angle required.'
+        else
+            call SysMsg('TEST_MESH: Remeshing due to angle required.')
+        endif
+        need_remeshing = 1
+    endif
   endif
 
   if( (dtout_screen .ne. 0)  .and. (need_remeshing .eq. 2) ) then
