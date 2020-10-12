e0a9b019e27cf196e43cb8f8b96f640d920a37ea
Eh Tan <tan2@earth.sinica.edu.tw>
2020-10-12 19:09:36 +0800

    ACC parallelize init_visc()

--- src/init_visc.f90
+++ src/init_visc.f90
@@ -1,10 +1,12 @@
 
 subroutine init_visc
+  !$ACC routine(Eff_visc) seq
   use arrays
   use params
   include 'precision.inc'
 
   !$OMP parallel do
+  !$ACC parallel loop collapse(2)
   do i = 1,nx-1
       do j = 1,nz-1
           visn(j,i) = Eff_visc(j,i)
