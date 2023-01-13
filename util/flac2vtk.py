02d1c8db2661066fbc3bb6f0c3fcbd607b833c8d
Eh Tan <tan2@earth.sinica.edu.tw>
2023-01-13 09:17:40 +0800

    Fix python3 warning

--- util/flac2vtk.py
+++ util/flac2vtk.py
@@ -185,7 +185,7 @@ def vts_dataarray(f, data, data_name=None, data_comps=None):
     if output_in_binary:
         header = np.zeros(4, dtype=np.int32)
         header[0] = 1
-        a = data.tostring()
+        a = data.tobytes()
         header[1] = len(a)
         header[2] = len(a)
         b = zlib.compress(a)
