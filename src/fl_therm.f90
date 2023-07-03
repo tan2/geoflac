tan2/geoflac

commit 30f2aa2b57ca19c5d479a3f340f15c47ed3668a5

Rework melting and fix some bugs
Bugs:
* Add OMP atomic
* Moho was shifted one element down
* Freezing was done on M(t+dt), not M(t)

src/fl_therm.f90
@@ -35,44 +35,92 @@ if (istress_therm.gt.0) then
 endif
 
 !$OMP Parallel private(i,j,iph,cp_eff,cond_eff,dissip,diff,quad_area, &
-!$OMP                  x1,x2,x3,x4,y1,y2,y3,y4,t1,t2,t3,t4,tmpr, &
-!$OMP                  ihw,delta_fmagma,qs,real_area13,area_n,rhs)
+!$OMP                  x1,x2,x3,x4,y1,y2,y3,y4,t1,t2,t3,t4,tmpr,fr_lambda, &
+!$OMP                  ihw,delta_fmagma,deltaT,qs,real_area13,area_n,rhs)
 
 if (itype_melting == 1) then
     ! M: fmegma, magma fraction in the element
-    ! dM/dt = P - M lambda
+    ! dM/dt = P - M * fr_lambda
+    ! Rearrange after forward Euler for dM/dt
+    ! M(t+dt) = M(t) + P * dt - M(t) * fr_lambda * dt
     !   where P is the production rate and the second term is the freezing rate
+    !   fr_lambda is temperature dependent: lower T freezes faster
+    ! fr_lambda = lambda_freeze * exp(-lambda_freeze_tdep * (T - Ttop))
+    ! P = fmelt * prod_magma * R_zone *  (A_zone / A_melt)
+    !   fmelt: melt fraction, calculated in chage_phase()
+    !   prod_magma: magma production rate, how rapid a melt migrate away
+    !               from the active melting region.
+    !   A_melt: the area of the melting element
+    !   R_zone, A_zone: the magma distribution ratio and the area of the two
+    !                   zones (crust and mantle). R_crust + R_mantle == 1.0
+    !   area_ratio = A_zone / A_melt
     !
-    ! Magma production rate:
-    ! M(t+dt) = M(t) + P dt
-    !   where P is the magma production rate.
-    ! P = fmelt * prod_magma * R_zone *  A_zone / A_melt
+    ! Magma freezing will release latent heat to the element
+    ! rho * cp * deltaT = rho * latent_heat * delta_fmagma
+    ! Rearrage to: deltaT = delta_fmagma * latent_heat / cp
+    ! This heat is distributed to the 4 corners evenly
     !
     !$OMP do
     !$ACC parallel loop collapse(2) async(1)
     do i = 1,nx-1
+        do j = 1,nz-1
+            !iph = iphase(j,i)
+            cp_eff = Eff_cp( j,i )
+            tmpr = 0.25d0*(temp(j,i)+temp(j+1,i)+temp(j,i+1)+temp(j+1,i+1))
+            fr_lambda = lambda_freeze * exp(-lambda_freeze_tdep * (tmpr-t_top))
+            delta_fmagma = max(fmagma(j,i), fmagma(j,i) * dt * fr_lambda)  !
+            fmagma(j,i) = fmagma(j,i) - delta_fmagma
+
+            ! latent heat released by freezing magma
+            deltaT = delta_fmagma * heat_latent_magma / cp_eff / 4
+            !$OMP atomic update
+            !$ACC atomic update
+            temp(j  ,i  ) = temp(j  ,i  ) + deltaT
+            !$OMP atomic update
+            !$ACC atomic update
+            temp(j  ,i+1) = temp(j  ,i+1) + deltaT
+            !$OMP atomic update
+            !$ACC atomic update
+            temp(j+1,i  ) = temp(j+1,i  ) + deltaT
+            !$OMP atomic update
+            !$ACC atomic update
+            temp(j+1,i+1) = temp(j+1,i+1) + deltaT
+        end do
+    enddo
+    !$OMP end do
+
+    !$OMP do
+    !$ACC parallel loop collapse(2) async(1)
+    do i = 1,nx-1
         ! XXX: Assume melting cannot happen above the moho. (j > jmoho(i)) is always true
+        ! starting from j=1, not jmoho(i), so that the loop can be collapsed and faster in computation
         do j = 1,nz-1
             jm = jmoho(i)
-            quad_area = 0.5d0/area(j,i,1) + 0.5d0/area(j,i,2) ! area of melting element
-            if (j>jm+1 .and. fmelt(j,i) > 0) then
+            quad_area = 0.5d0/area(j,i,1) + 0.5d0/area(j,i,2) ! area of this element
+            if (j>jm .and. fmelt(j,i) > 0) then
+                ! This element is under melting. The melt will migrate to the
+                ! mantle and crust above. Treat the migration as instantaneous.
+
                 ! Within crust, melts migrate by diking, propagate upward vertically
                 ! area_ratio: the area of the crust column / the area of the melting element
                 !     ~ the thickness of the crust column / the thickness of the melting element
-                area_ratio = (cord(1,i,2)+cord(1,i+1,2)-cord(jm+1,i,2)-cord(jm+1,i+1,2)) / &
+                area_ratio = (cord(1,i,2)+cord(1,i+1,2)-cord(jm,i,2)-cord(jm,i+1,2)) / &
                     (cord(j,i,2)+cord(j,i+1,2)-cord(j+1,i,2)-cord(j+1,i+1,2))
                 do jj = 1, jm
+                    !$OMP atomic update
                     !$ACC atomic update
-                    fmagma(jj,i) = fmagma(jj,i) + fmelt(j,i) * ratio_crust_mzone * area_ratio * prod_magma * dt
+                    fmagma(jj,i) = fmagma(jj,i) + fmelt(j,i) * (1d0 - ratio_mantle_mzone) * area_ratio * prod_magma * dt
                 enddo
+
                 ! Within mantle, melts migrate by percolation, propagate upward slantly
-                depth_moho = 0.5d0 * (cord(jm+1,i,2) + cord(jm+1,i+1,2))
+                depth_moho = 0.5d0 * (cord(jm,i,2) + cord(jm,i+1,2))
                 ! area_ratio: the area of the mantle triangle / the area of the melting element
                 area_ratio = 0.5d0 * (depth_moho - 0.5d0 *(cord(j,i,2)+cord(j,i+1,2))) * width_mzone / quad_area
                 do ii = max(1,i-ihalfwidth_mzone), min(nx-1,i+ihalfwidth_mzone)
                     do jj = jmoho(ii)+1, j
                         ihw = ihalfwidth_mzone * (j - jj + 1) / (j - jmoho(ii) + 1)
                         if (abs(ii-i) <= ihw) then
+                            !$OMP atomic update
                             !$ACC atomic update
                             fmagma(jj,ii) = fmagma(jj,ii) + fmelt(j,i) * ratio_mantle_mzone * area_ratio * prod_magma * dt
                         endif
@@ -82,37 +130,6 @@ if (itype_melting == 1) then
             fmagma(j,i) = min(fmagma(j,i), fmagma_max)
         enddo
     enddo
-
-    ! Magma freezing rate:
-    ! [M(t+dt) - M(t)] / dt  = - M(t) lambda
-    ! M(t+dt) = M(t) (1 - dt * lambda)
-    !   where lambda is temperature dependent: low T has faster decay
-    !   lambda = lambda_freeze * exp(-lambda_freeze_tdep * (T - Tsolidus))
-    !
-    !$OMP do
-    !$ACC parallel loop collapse(2) async(1)
-    do i = 1,nx-1
-        do j = 1,nz-1
-            !iph = iphase(j,i)
-            cp_eff = Eff_cp( j,i )
-            tmpr = 0.25d0*(temp(j,i)+temp(j+1,i)+temp(j,i+1)+temp(j+1,i+1))
-
-            fmagma_old = fmagma(j,i)
-            delta_fmagma = fmagma(j,i) * dt * lambda_freeze * exp(-lambda_freeze_tdep * (tmpr-t_top))
-            fmagma(j,i) = max(fmagma(j,i) - delta_fmagma, 0d0)
-
-            ! latent heat released by freezing magma
-            !$ACC atomic update
-            temp(j  ,i  ) = temp(j  ,i  ) + delta_fmagma * heat_latent_magma / cp_eff / 4
-            !$ACC atomic update
-            temp(j  ,i+1) = temp(j  ,i+1) + delta_fmagma * heat_latent_magma / cp_eff / 4
-            !$ACC atomic update
-            temp(j+1,i  ) = temp(j+1,i  ) + delta_fmagma * heat_latent_magma / cp_eff / 4
-            !$ACC atomic update
-            temp(j+1,i+1) = temp(j+1,i+1) + delta_fmagma * heat_latent_magma / cp_eff / 4
-        end do
-    enddo
-    !$OMP end do
 endif
 
 !$ACC parallel loop collapse(2) async(1)

commit 0140737c45a5c779b723339e5d360143018a0e8b

Fix a bug in magma freezing
The bug was introduced in 30f2aa2b57 and is reported by Chi-Hsin Chen.

src/fl_therm.f90
@@ -71,7 +71,7 @@ if (itype_melting == 1) then
             cp_eff = Eff_cp( j,i )
             tmpr = 0.25d0*(temp0(j,i)+temp0(j+1,i)+temp0(j,i+1)+temp0(j+1,i+1))
             fr_lambda = lambda_freeze * exp(-lambda_freeze_tdep * (tmpr-t_top))
-            delta_fmagma = max(fmagma(j,i), fmagma(j,i) * dt * fr_lambda)  !
+            delta_fmagma = min(fmagma(j,i), fmagma(j,i) * dt * fr_lambda)
             fmagma(j,i) = fmagma(j,i) - delta_fmagma
 
             ! latent heat released by freezing magma