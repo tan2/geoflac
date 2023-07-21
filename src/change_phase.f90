tan2/geoflac

commit f303156acc8bb0e59204ee1f8291f0f03ecac743

Modify schist phase diagram

src/change_phase.f90
@@ -150,8 +150,11 @@ do kk = 1 , nmarkers
         mark_phase(kk) = kmetased
     case (kmetased)
         ! dehydration, sedimentary rock -> schist
-        ! from sediment solidus in Nichols et al., Nature, 1994
-        if (tmpr < 650d0 .or. depth < 20d3) cycle
+        ! from metapelite KFMASH petrogenic grid,
+        ! invariant points i5 (710.8 C, 0.880 GPa) i3 (662.3 C, 0.704 GPa)
+        ! Fig 1, Wei, Powell, Clarke, J. Metamorph. Geol., 2004.
+        trpres = 0.88d9 + (0.88d9 - 0.704d9) * (710.8d0 - tmpr) / (710.8d0 - 662.3d0)
+        if (press < trpres ) cycle
         !$ACC atomic write
         !$OMP atomic write
         itmp(j,i) = 1

commit e6d2fafebbe3d887df053e749e86a72d24b77144

Fix a bug in metased -> schist
Bug introduced in f303156acc

src/change_phase.f90
@@ -153,7 +153,7 @@ do kk = 1 , nmarkers
         ! from metapelite KFMASH petrogenic grid,
         ! invariant points i5 (710.8 C, 0.880 GPa) i3 (662.3 C, 0.704 GPa)
         ! Fig 1, Wei, Powell, Clarke, J. Metamorph. Geol., 2004.
-        trpres = 0.88d9 + (0.88d9 - 0.704d9) * (710.8d0 - tmpr) / (710.8d0 - 662.3d0)
+        trpres = 0.88d9 - (0.88d9 - 0.704d9) * (710.8d0 - tmpr) / (710.8d0 - 662.3d0)
         if (press < trpres ) cycle
         !$ACC atomic write
         !$OMP atomic write