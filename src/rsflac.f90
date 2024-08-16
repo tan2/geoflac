tan2/geoflac

commit 374a3a0e84b90759ab9f8feceed4c436a387628f

Input angle, not width, of mzone

src/rsflac.f90
@@ -33,6 +33,10 @@ open (1,file='cord.rs',access='direct',recl=nwords*kindr)
 read (1,rec=nrec) cord
 close (1)
 
+! min element width and thickness
+dxmin = minval(cord(1,2:nx,1) - cord(1,1:nx-1,1))
+dzmin = minval(cord(2,1,1:nz-1) - cord(2,1,2:nz))
+
 open (1,file='dhacc.rs',access='direct',recl=(nx-1)*kindr)
 read (1,rec=nrec) dhacc(1:nx-1)
 close (1)

src/fl_therm.f90
@@ -10,6 +10,10 @@ include 'precision.inc'
 
 double precision :: D(3,3)  ! diffusion operator
 
+tan_mzone = tand(0.5d0 * angle_mzone)
+! max. width of the magma zone @ moho (as if melting occurs at 200 km)
+ihalfwidth_mzone = ceiling(tan_mzone * 200e3 / dxmin)
+
 ! real_area = 0.5* (1./area(n,t))
 ! Calculate Fluxes in every triangle
 !      flux (num_triangle, direction(x,y), j, i)
@@ -35,7 +39,8 @@ endif
 
 !$OMP Parallel private(i,j,iph,cp_eff,cond_eff,dissip,diff,quad_area, &
 !$OMP                  x1,x2,x3,x4,y1,y2,y3,y4,t1,t2,t3,t4,tmpr,fr_lambda, &
-!$OMP                  ihw,delta_fmagma,deltaT,qs,real_area13,area_n,rhs)
+!$OMP                  delta_fmagma,deltaT,qs,real_area13,area_n,rhs, &
+!$OMP                  jm,area_ratio,z_moho,z_melt,x_melt,h,x,z)
 
 if (itype_melting == 1) then
     ! M: fmegma, magma fraction in the element
@@ -63,7 +68,6 @@ if (itype_melting == 1) then
     !$ACC parallel loop collapse(2) async(1)
     do i = 1,nx-1
         do j = 1,nz-1
-            !iph = iphase(j,i)
             cp_eff = Eff_cp( j,i )
             tmpr = 0.25d0*(temp0(j,i)+temp0(j+1,i)+temp0(j,i+1)+temp0(j+1,i+1))
             fr_lambda = lambda_freeze * exp(-lambda_freeze_tdep * (tmpr-t_top))
@@ -112,13 +116,18 @@ if (itype_melting == 1) then
                 enddo
 
                 ! Within mantle, melts migrate by percolation, propagate upward slantly
-                depth_moho = 0.5d0 * (cord(jm,i,2) + cord(jm,i+1,2))
+                z_moho = 0.5d0 * (cord(jm,i,2) + cord(jm,i+1,2))
+                z_melt = 0.5d0 * (cord(j+1,i,2) + cord(j+1,i+1,2))
+                x_melt = 0.5d0 * (cord(j+1,i,1) + cord(j+1,i+1,1))
+                h = z_moho - z_melt
                 ! area_ratio: the area of the mantle triangle / the area of the melting element
-                area_ratio = 0.5d0 * (depth_moho - 0.5d0 *(cord(j,i,2)+cord(j,i+1,2))) * width_mzone / quad_area
+                area_ratio = h * h * tan_mzone / quad_area
+                ! ii: the potential region of magma distribution zone at moho
                 do ii = max(1,i-ihalfwidth_mzone), min(nx-1,i+ihalfwidth_mzone)
                     do jj = jmoho(ii)+1, j
-                        ihw = ihalfwidth_mzone * (j - jj + 1) / (j - jmoho(ii) + 1)
-                        if (abs(ii-i) <= ihw) then
+                        x = 0.5d0 * (cord(jj,ii,1) + cord(jj,ii+1,1))
+                        z = 0.5d0 * (cord(jj,ii,2) + cord(jj,ii+1,2))
+                        if (abs(x - x_melt) <= tan_mzone * (z - z_melt)) then
                             !$OMP atomic update
                             !$ACC atomic update
                             fmagma(jj,ii) = fmagma(jj,ii) + fmelt(j,i) * ratio_mantle_mzone * area_ratio * prod_magma * dt

src/init_cord.f90
@@ -67,11 +67,9 @@ enddo
 
 200 continue
 
-! mid element width
+! min element width and thickness
 dxmin = minval(cord(1,2:nx,1) - cord(1,1:nx-1,1))
-
-ihalfwidth_mzone = int(width_mzone / 2 / dxmin)
-
+dzmin = minval(cord(1:nz-1,1,2) - cord(2:nz,1,2))
 
 dhacc(:) = 0.d0
 extr_acc(1:nx-1) = 0.d0

src/params.f90
@@ -31,7 +31,6 @@ integer :: nx,nz,nzonx,nzony,nelz_x(maxzone),nelz_y(maxzone), &
      nph_layer(maxzone_age),iph_col(maxzone_age, maxzone_layer), &
      iph_col_trans(maxzone_age), &
      if_hydro,nyhydro,iphsub, &
-     ihalfwidth_mzone, &
      movegrid,ndim,ifreq_visc,i_rey, &
      incoming_left,incoming_right, &
      ivis_present,idt_scale,ifreq_imasses,ifreq_rmasses, &
@@ -56,7 +55,6 @@ integer :: nx,nz,nzonx,nzony,nelz_x(maxzone),nelz_y(maxzone), &
 !$ACC     nph_layer(maxzone_age),iph_col1(maxzone_age, maxzone_layer), &
 !$ACC     iph_col_trans(maxzone_age), &
 !$ACC     if_hydro,nyhydro,iphsub, &
-!$ACC     ihalfwidth_mzone, &
 !$ACC     movegrid,ndim,ifreq_visc,i_rey, &
 !$ACC     incoming_left,incoming_right, &
 !$ACC     ivis_present,idt_scale,ifreq_imasses,ifreq_rmasses, &
@@ -65,6 +63,7 @@ integer :: nx,nz,nzonx,nzony,nelz_x(maxzone),nelz_y(maxzone), &
 real*8 :: x0,z0,rxbo,rzbo,sizez_x(maxzone),sizez_y(maxzone), &
      dx_rem,angle_rem,topo_kappa,fac_kappa, &
      v_min,v_max,efoldc, &
+     dxmin,dzmin, &
      prod_magma,rho_magma, &
      bca(maxbc),bcb(maxbc),bcc(maxbc),xReyn, &
      bcd(maxbc),bce(maxbc),bcf(maxbc),bcg(maxbc),bch(maxbc),bci(maxbc), &
@@ -84,7 +83,7 @@ real*8 :: x0,z0,rxbo,rzbo,sizez_x(maxzone),sizez_y(maxzone), &
      hc(maxzone_age,maxzone_layer), &
      age_1(maxzone_age),tp1(maxzone_age),tp2(maxzone_age), &
      g,pisos,drosub,damp_vis, &
-     width_mzone,fmagma_max,ratio_crust_mzone,ratio_mantle_mzone, &
+     angle_mzone,fmagma_max,ratio_crust_mzone,ratio_mantle_mzone, &
      latent_heat_magma,lambda_freeze,lambda_freeze_tdep, &
      weaken_ratio_plastic,weaken_ratio_viscous, &
      dtavg, &
@@ -93,6 +92,7 @@ real*8 :: x0,z0,rxbo,rzbo,sizez_x(maxzone),sizez_y(maxzone), &
 !$ACC declare create(x0,z0,rxbo,rzbo,sizez_x(maxzone),sizez_y(maxzone), &
 !$ACC     dx_rem,angle_rem,topo_kappa,fac_kappa, &
 !$ACC     v_min,v_max,efoldc, &
+!$ACC     dxmin,dzmin, &
 !$ACC     prod_magma,rho_magma, &
 !$ACC     bca(maxbc),bcb(maxbc),bcc(maxbc),xReyn, &
 !$ACC     bcd(maxbc),bce(maxbc),bcf(maxbc),bcg(maxbc),bch(maxbc),bci(maxbc), &
@@ -112,7 +112,7 @@ real*8 :: x0,z0,rxbo,rzbo,sizez_x(maxzone),sizez_y(maxzone), &
 !$ACC     hc(maxzone_age,maxzone_layer), &
 !$ACC     age_1(maxzone_age),tp1(maxzone_age),tp2(maxzone_age), &
 !$ACC     g,pisos,drosub,damp_vis, &
-!$ACC     width_mzone,fmagma_max,ratio_crust_mzone,ratio_mantle_mzone, &
+!$ACC     angle_mzone,fmagma_max,ratio_crust_mzone,ratio_mantle_mzone, &
 !$ACC     latent_heat_magma,lambda_freeze,lambda_freeze_tdep, &
 !$ACC     weaken_ratio_plastic,weaken_ratio_viscous, &
 !$ACC     dtavg, &

src/read_params.f90
@@ -201,7 +201,7 @@ read(4,*) v_min, v_max, ivis_shape,efoldc
 call AdvanceToNextInputLine( 4 )
 read(4,*) itype_melting, nelem_serp, prod_magma, rho_magma
 call AdvanceToNextInputLine( 4 )
-read(4,*) width_mzone, fmagma_max, ratio_crust_mzone, ratio_mantle_mzone
+read(4,*) angle_mzone, fmagma_max, ratio_crust_mzone, ratio_mantle_mzone
 call AdvanceToNextInputLine( 4 )
 read(4,*) latent_heat_magma, lambda_freeze, lambda_freeze_tdep
 call AdvanceToNextInputLine( 4 )

src/setflac.f90
@@ -94,7 +94,6 @@ include 'precision.inc'
 !$ACC     nph_layer(maxzone_age),iph_col1(maxzone_age, maxzone_layer), &
 !$ACC     iph_col_trans(maxzone_age), &
 !$ACC     if_hydro,nyhydro,iphsub, &
-!$ACC     ihalfwidth_mzone, &
 !$ACC     movegrid,ndim,ifreq_visc,i_rey, &
 !$ACC     incoming_left,incoming_right, &
 !$ACC     ivis_present,idt_scale,ifreq_imasses,ifreq_rmasses, &
@@ -103,6 +102,7 @@ include 'precision.inc'
 !$ACC update device(x0,z0,rxbo,rzbo,sizez_x(maxzone),sizez_y(maxzone), &
 !$ACC     dx_rem,angle_rem,topo_kappa,fac_kappa, &
 !$ACC     v_min,v_max,efoldc, &
+!$ACC     dxmin,dzmin, &
 !$ACC     prod_magma,rho_magma, &
 !$ACC     bca(maxbc),bcb(maxbc),bcc(maxbc),xReyn, &
 !$ACC     bcd(maxbc),bce(maxbc),bcf(maxbc),bcg(maxbc),bch(maxbc),bci(maxbc), &
@@ -122,7 +122,7 @@ include 'precision.inc'
 !$ACC     hc(maxzone_age,maxzone_layer), &
 !$ACC     age_1(maxzone_age),tp1(maxzone_age),tp2(maxzone_age), &
 !$ACC     g,pisos,drosub,damp_vis, &
-!$ACC     width_mzone,fmagma_max,ratio_crust_mzone,ratio_mantle_mzone, &
+!$ACC     angle_mzone,fmagma_max,ratio_crust_mzone,ratio_mantle_mzone, &
 !$ACC     latent_heat_magma,lambda_freeze,lambda_freeze_tdep, &
 !$ACC     weaken_ratio_plastic,weaken_ratio_viscous, &
 !$ACC     dtavg, &

commit a4040ab2b3ea90f0fda57109324682c37fb4ff93

Fix a bug in restart
Bug introduced in 374a3a0e84

src/rsflac.f90
@@ -35,13 +35,12 @@ close (1)
 
 ! min element width and thickness
 dxmin = minval(cord(1,2:nx,1) - cord(1,1:nx-1,1))
-dzmin = minval(cord(2,1,1:nz-1) - cord(2,1,2:nz))
+dzmin = minval(cord(1:nz-1,1,2) - cord(2:nz,1,2))
 
 open (1,file='dhacc.rs',access='direct',recl=(nx-1)*kindr)
 read (1,rec=nrec) dhacc(1:nx-1)
 close (1)
 
-open (1,file='extr_acc.rs',access='direct',recl=(nx-1)*kindr)
 read (1,rec=nrec) extr_acc(1:nx-1)
 close (1)
 