49579a9a65140c20fda41112e3816aacfa7e985b
Chase J. Shyu <iamhemry@gmail.com>
2025-04-08 08:56:22 -0500

    update python env path

--- util/flac2vtk.py
+++ util/flac2vtk.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 '''Convert the binary output of flac to VTK (vts) files.
 '''
--- util/flacmarker2vtk.py
+++ util/flacmarker2vtk.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 '''Convert the binary marker output of flac to VTK (vtp) files.
 
