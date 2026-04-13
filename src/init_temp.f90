tan2/geoflac

commit 4f675f625a872048d0f68c2f9655be26d0de4393

Refactor sidewalltemp()

src/init_temp.f90
@@ -5,8 +5,6 @@ use arrays
 use params
 include 'precision.inc'
 
-double precision, parameter :: pi = 3.14159265358979323846d0
-
 !  Read distribution of temperatures from the dat file
 if (irtemp .gt. 0) then
     open( 1, file=tempfile, status='old', err=101 )
@@ -31,145 +29,21 @@ do n = 1, nzone_age
         if (iph_col_trans(n-1) == 1) cycle
     endif
 
-    if (ictherm(n)==1) then
-        !! Oceanic geotherm (half space cooling model, T&S 3rd ed. Eq(4.113))
-        diffusivity = 1.d-6
-        do i = ixtb1(n), ixtb2(n)
-            age = age_1(n)
-            if (iph_col_trans(n) == 1) then
-                i1 = ixtb1(n)
-                i2 = ixtb2(n)
-                ratio = (cord(1,i,1) - cord(1,i1,1)) / (cord(1,i2,1) - cord(1,i1,1))
-                age = age_1(n) + (age_1(n+1) - age_1(n)) * ratio
-            endif
-            do j = 1,nz
-                f = (cord(1,i,2)-cord(j,i,2)) / sqrt(4 * diffusivity * age * 1.d6 * sec_year)
-                temp(j,i) = t_top + (t_bot - t_top) * erf(f)
-                !print *, j, age, -cord(j,i,2), temp(j,i)
-            enddo
-        enddo
-    elseif (ictherm(n)==2) then
-        !! Oceanic geotherm (plate cooling cooling model, T&S 3rd ed. Eq(4.130))
-        diffusivity = 1.d-6
-        do i = ixtb1(n), ixtb2(n)
-            age = age_1(n)
-            yL0 = tp1(n)    ! plate thickness in km
-            if (iph_col_trans(n) == 1) then
-                i1 = ixtb1(n)
-                i2 = ixtb2(n)
-                ratio = (cord(1,i,1) - cord(1,i1,1)) / (cord(1,i2,1) - cord(1,i1,1))
-                age = age_1(n) + (age_1(n+1) - age_1(n)) * ratio
-                yL0 = tp1(n) + (tp1(n+1) - tp1(n)) * ratio
-            endif
-            age_init = age*sec_year*1d6
-            tau_d = yL0*yL0*1d6 / (pi*pi*diffusivity)
-            do j = 1,nz
-                ! depth in km
-                y = (cord(1,i,2) - cord(j,i,2)) * 1d-3
-                tss = t_top + (t_bot - t_top) * y / yL0
-                tt = 0.d0
-                do k = 1,100
-                    tt = tt + 1.d0/k * exp(-k*k*age_init/tau_d) * sin(pi*k*y/yL0)
-                enddo
-                temp(j,i) = tss + 2.d0/pi*(t_bot-t_top)*tt
-                if(temp(j,i)>t_bot .or. y>yL0) temp(j,i) = t_bot
-            enddo
-        enddo
-    elseif (ictherm(n)==12) then
-        !! Continental geotherm (plate cooling model with radiogenic heating)
-        !
-        ! Starting from the steady state (ss) solution as in T&S 3rd ed. Eq(4.30)
-        ! Let the ss moho temperature be tm and ss heatflux be qm.
-        ! qm = cond * (t_bot - tm) / (yL0 - ymoho)
-        ! Substitute qm to 4.30 to solve for tm
-        age = age_1(n)
-        yL0 = tp1(n)    ! plate thickness in km
-        ymoho = tp2(n)  ! crust thickness in km
-        cond = 3.3d0
-        dens_c = 2700.d0
-        diffusivity = 1.d-6
-        !   write(*,*) rzbo, hs, hr
-        do i = ixtb1(n), ixtb2(n)
-            if (iph_col_trans(n) == 1) then
-                i1 = ixtb1(n)
-                i2 = ixtb2(n)
-                ratio = (cord(1,i,1) - cord(1,i1,1)) / (cord(1,i2,1) - cord(1,i1,1))
-                age = age_1(n) + (age_1(n+1) - age_1(n)) * ratio
-                yL0 = tp1(n) + (tp1(n+1) - tp1(n)) * ratio
-                ymoho = tp2(n) + (tp2(n+1) - tp2(n)) * ratio
-            endif
-            age_init = age*sec_year*1d6
-            tau_d = yL0*yL0*1d6 / (pi*pi*diffusivity)
-            rr = ymoho / (yL0 - ymoho)
-            tm = (t_top + rr*t_bot + dens_c*hs*hr*hr*1d6/cond*(1d0-exp(-ymoho/hr))) / (1 + rr)
-            qm = cond * (t_bot - tm) / (yL0 - ymoho)
-            do j = 1,nz
-                ! depth in km
-                y = (cord(1,i,2) - cord(j,i,2)) * 1d-3
-
-                ! ss part with radiogenic heat
-                if (y <= ymoho) then
-                    tss = t_top + qm/cond*y + (dens_c*hs*hr*hr*1.d+6/cond)*(1d0-exp(-exp(-y/hr)))
-                elseif (y <= yL0) then ! below moho, inside lithosphere
-                    tss = tm + qm/cond*(y-ymoho)
-                else
-                    tss = t_bot
-                endif
-                ! time-dependent part
-                ! see T&S 3rd ed. Eq(4.130)
-                tt = 0.d0
-                do k = 1,100
-                    tt = tt + 1.d0/k * exp(-k*k*age_init/tau_d) * sin(pi*k*y/yL0)
-                enddo
-                temp(j,i) = tss + 2.d0/pi*(t_bot-t_top)*tt
-                if(temp(j,i)>t_bot .or. y>yL0) temp(j,i) = t_bot
-                !       write(*,*) j,y,tss,yL0,tt
-            enddo
-        enddo
-    elseif (ictherm(n)==21) then
-        !! Constant geotherm gradient at top layer, then T=t_bot all the way to the bottom
-        bot_dep = age_1(n)   ! bottom of the top layer
-        do i = ixtb1(n), ixtb2(n)
-            if (iph_col_trans(n) == 1) then
-                i1 = ixtb1(n)
-                i2 = ixtb2(n)
-                ratio = (cord(1,i,1) - cord(1,i1,1)) / (cord(1,i2,1) - cord(1,i1,1))
-                bot_dep = age_1(n) + (age_1(n+1) - age_1(n)) * ratio
-            endif
-            do j = 1,nz
-                y = (cord(1,i,2)-cord(j,i,2))*1.d-3
-                temp(j,i) = y * (t_bot-t_top) / bot_dep
-                if(temp(j,i).gt.t_bot) temp(j,i) = t_bot
-            enddo
-        enddo
-    elseif (ictherm(n)==22) then
-        !! Constant geotherm gradient at top two layers, then T=t_bot all the way to the bottom
-        temp1 = age_1(n)
-        ylayer1 = tp1(n)  ! depth in km
-        ylayer2 = tp2(n)  ! depth in km
-        do i = ixtb1(n), ixtb2(n)
-            if ((iph_col_trans(n) == 1))then
-                i1 = ixtb1(n)
-                i2 = ixtb2(n)
-                ratio = (cord(1,i,1) - cord(1,i1,1)) / (cord(1,i2,1) - cord(1,i1,1))
-                temp1 = age_1(n) + (age_1(n+1) - age_1(n)) * ratio
-                ylayer1 = tp1(n) + (tp1(n+1) - tp1(n)) * ratio
-                ylayer2 = tp2(n) + (tp2(n+1) - tp2(n)) * ratio
-            endif
-            do j = 1,nz
-                y = (cord(1,i,2)-cord(j,i,2))*1.d-3
-                if (y <= ylayer1) then
-                    temp(j,i) = t_top + (temp1 - t_top) * y / ylayer1
-                else
-                    temp(j,i) = temp1 + (t_bot - temp1) * (y - ylayer1) / (ylayer2 - ylayer1)
-                endif
-                if(temp(j,i).gt.t_bot) temp(j,i) = t_bot
-            enddo
-        enddo
-    else
-        call sysmsg('init_temp: ictherm not supported!')
-        stop 1
-    endif
+    do i = ixtb1(n), ixtb2(n)
+        if (iph_col_trans(n) == 1) then
+            i1 = ixtb1(n)
+            i2 = ixtb2(n)
+            ratio = (cord(1,i,1) - cord(1,i1,1)) / (cord(1,i2,1) - cord(1,i1,1))
+            age_1n = age_1(n) + (age_1(n+1) - age_1(n)) * ratio
+            tp1n = tp1(n) + (tp1(n+1) - tp1(n)) * ratio
+            tp2n = tp2(n) + (tp2(n+1) - tp2(n)) * ratio
+        else
+            age_1n = age_1(n)
+            tp1n = tp1(n)
+            tp2n = tp2(n)
+        endif
+        call init_geotherm(n, i, age_1n, tp1n, tp2n)
+    enddo
 enddo
 
 10 continue
@@ -247,26 +121,127 @@ return
 end subroutine init_temp
 
 
+subroutine init_geotherm(n, i, age_1n, tp1n, tp2n)
+    !$ACC routine vector
+    use arrays
+    use params
+    include 'precision.inc'
+
+    double precision, parameter :: pi = 3.14159265358979323846d0
+
+
+    if (ictherm(n)==1) then
+        !! Oceanic geotherm (half space cooling model, T&S 3rd ed. Eq(4.113))
+        diffusivity = 1.d-6
+        age = age_1n
+        !$ACC loop
+        do j = 1,nz
+            f = (cord(1,i,2)-cord(j,i,2)) / sqrt(4 * diffusivity * age * 1.d6 * sec_year)
+            temp(j,i) = t_top + (t_bot - t_top) * erf(f)
+            !print *, j, age, -cord(j,i,2), temp(j,i)
+        enddo
+    elseif (ictherm(n)==2) then
+        !! Oceanic geotherm (plate cooling cooling model, T&S 3rd ed. Eq(4.130))
+        diffusivity = 1.d-6
+        age = age_1n
+        yL0 = tp1n    ! plate thickness in km
+        age_init = age*sec_year*1d6
+        tau_d = yL0*yL0*1d6 / (pi*pi*diffusivity)
+        !$ACC loop
+        do j = 1,nz
+            ! depth in km
+            y = (cord(1,i,2) - cord(j,i,2)) * 1d-3
+            tss = t_top + (t_bot - t_top) * y / yL0
+            tt = 0.d0
+            do k = 1,100
+                tt = tt + 1.d0/k * exp(-k*k*age_init/tau_d) * sin(pi*k*y/yL0)
+            enddo
+            temp(j,i) = tss + 2.d0/pi*(t_bot-t_top)*tt
+            if(temp(j,i)>t_bot .or. y>yL0) temp(j,i) = t_bot
+        enddo
+    elseif (ictherm(n)==12) then
+        !! Continental geotherm (plate cooling model with radiogenic heating)
+        !
+        ! Starting from the steady state (ss) solution as in T&S 3rd ed. Eq(4.30)
+        ! Let the ss moho temperature be tm and ss heatflux be qm.
+        ! qm = cond * (t_bot - tm) / (yL0 - ymoho)
+        ! Substitute qm to 4.30 to solve for tm
+        age = age_1n
+        yL0 = tp1n    ! plate thickness in km
+        ymoho = tp2n  ! crust thickness in km
+        cond = 3.3d0
+        dens_c = 2700.d0
+        diffusivity = 1.d-6
+        !   write(*,*) rzbo, hs, hr
+        age_init = age*sec_year*1d6
+        tau_d = yL0*yL0*1d6 / (pi*pi*diffusivity)
+        rr = ymoho / (yL0 - ymoho)
+        tm = (t_top + rr*t_bot + dens_c*hs*hr*hr*1d6/cond*(1d0-exp(-ymoho/hr))) / (1 + rr)
+        qm = cond * (t_bot - tm) / (yL0 - ymoho)
+        !$ACC loop
+        do j = 1,nz
+            ! depth in km
+            y = (cord(1,i,2) - cord(j,i,2)) * 1d-3
+
+            ! ss part with radiogenic heat
+            if (y <= ymoho) then
+                tss = t_top + qm/cond*y + (dens_c*hs*hr*hr*1.d+6/cond)*(1d0-exp(-exp(-y/hr)))
+            elseif (y <= yL0) then ! below moho, inside lithosphere
+                tss = tm + qm/cond*(y-ymoho)
+            else
+                tss = t_bot
+            endif
+            ! time-dependent part
+            ! see T&S 3rd ed. Eq(4.130)
+            tt = 0.d0
+            do k = 1,100
+                tt = tt + 1.d0/k * exp(-k*k*age_init/tau_d) * sin(pi*k*y/yL0)
+            enddo
+            temp(j,i) = tss + 2.d0/pi*(t_bot-t_top)*tt
+            if(temp(j,i)>t_bot .or. y>yL0) temp(j,i) = t_bot
+            !       write(*,*) j,y,tss,yL0,tt
+        enddo
+    elseif (ictherm(n)==21) then
+        !! Constant geotherm gradient at top layer, then T=t_bot all the way to the bottom
+        bot_dep = age_1n   ! bottom of the top layer
+        do j = 1,nz
+            y = (cord(1,i,2)-cord(j,i,2))*1.d-3
+            temp(j,i) = y * (t_bot-t_top) / bot_dep
+            if(temp(j,i).gt.t_bot) temp(j,i) = t_bot
+        enddo
+    elseif (ictherm(n)==22) then
+        !! Constant geotherm gradient at top two layers, then T=t_bot all the way to the bottom
+        temp1 = age_1n
+        ylayer1 = tp1n  ! depth in km
+        ylayer2 = tp2n  ! depth in km
+        !$ACC loop
+        do j = 1,nz
+            y = (cord(1,i,2)-cord(j,i,2))*1.d-3
+            if (y <= ylayer1) then
+                temp(j,i) = t_top + (temp1 - t_top) * y / ylayer1
+            else
+                temp(j,i) = temp1 + (t_bot - temp1) * (y - ylayer1) / (ylayer2 - ylayer1)
+            endif
+            if(temp(j,i).gt.t_bot) temp(j,i) = t_bot
+        enddo
+    else
+        !call sysmsg('init_temp: ictherm not supported!')
+        stop 1
+    endif
+end subroutine init_geotherm
+
+
 subroutine sidewalltemp(i1, i2)
+  !$ACC routine(init_geotherm) vector
+
   use arrays, only : temp, cord, source
   use params
   implicit none
   ! This subroutine is intended for remeshing.
 
   integer, intent(in) :: i1, i2
-  double precision :: cond_c, cond_m, dens_c, dens_m, pi, diffusivity, y
-  integer :: n, i, j
-  
-  cond_c = 2.2d0
-  cond_m = 3.3d0
-  dens_c = 2700.d0
-  dens_m = 3300.d0
-  pi = 3.14159d0
-  diffusivity = 1.d-6
-
-  if(nzone_age < 1) then
-      stop 'nzone_age < 1, cannot determine temperature of incoming material'
-  endif
+  double precision :: age_1n, tp1n, tp2n
+  integer :: n, i
 
   if(i1 == 1) then
       ! left sidewall
@@ -275,51 +250,14 @@ subroutine sidewalltemp(i1, i2)
       ! right sidewall
       n = nzone_age
   endif
+  age_1n = age_1(n)
+  tp1n = tp1(n)
+  tp2n = tp2(n)
 
-!!$  if(iph_col3(n)==kocean1 .or. iph_col3(n)==kocean2) then
-      !! Oceanic geotherm (half space cooling model)
-      !$ACC parallel loop collapse(2) async(1)
-      do i = i1, i2
-          do j = 1,nz
-              ! depth in km
-              y = (cord(1,i,2)-cord(j,i,2)) / sqrt(4 * diffusivity * age_1(n) * 1.d6 * sec_year)
-              temp(j,i) = t_top + (t_bot - t_top) * erf(y)
-              !print *, j, age_1(n), -cord(j,i,2), temp(j,i)
-          enddo
-      enddo
-!!$  else
-!!$      !! Continental geotherm
-!!$      tr= dens_c*hs*hr*hr*1.d+6/cond_c*exp(1.-exp(-hc3(n)/hr))
-!!$      q_m = (t_bot-t_top-tr)/((hc3(n)*1000.d0)/cond_c+((200.d3-(hc3(n))*1000.d0))/cond_m)
-!!$      tm  = t_top + (q_m/cond_c)*hc3(n)*1000.d0 + tr
-!!$      !   write(*,*) rzbo, tr, hs, hr, hc3(n), q_m, tm
-!!$      age_init = age_1(n)*3.14d0*1.d+7*1.d+6 + time
-!!$      diff_m = cond_m/1000.d0/dens_m
-!!$      tau_d = 200.d3*200.d3/(pi*pi*diff_m)
-!!$
-!!$      do i = i1, i2
-!!$          do j = 1,nz
-!!$              ! depth in km
-!!$              y = (cord(1,i,2)-cord(j,i,2))*1.d-3
-!!$              !  steady state part
-!!$              if (y.le.hc3(n)) tss = t_top+(q_m/cond_c)*y*1000.d0+(dens_c*hs*hr*hr*1.d+6/cond_c)*exp(1.d0-exp(-y/hr))
-!!$              if (y.gt.hc3(n)) tss = tm + (q_m/cond_m)*1000.d0*(y-hc3(n))
-!!$
-!!$              ! time-dependent part
-!!$              tt = 0.d0
-!!$              pp =-1.d0
-!!$              do k = 1,100
-!!$                  an = 1.d0*k
-!!$                  pp = -pp
-!!$                  tt = tt +pp/(an)*exp(-an*an*age_init/tau_d)*dsin(pi*k*(200.d3-y*1000.d0)/(200.d3))
-!!$              enddo
-!!$              temp(j,i) = tss +2.d0/pi*(t_bot-t_top)*tt
-!!$              if(temp(j,i).gt.1330.or.y.gt.200.d0) temp(j,i)= 1330.d0
-!!$              if (j.eq.1) temp(j,i) = t_top
-!!$              !       write(*,*) tss,tm,q_m,cond_m,hc3(n),y,tt
-!!$          enddo
-!!$      enddo
-!!$  endif
+  !$ACC parallel loop async(1)
+  do i = i1, i2
+    call init_geotherm(n, i, age_1n, tp1n, tp2n)
+  enddo
 
   if(i1 == 1) then
       !$ACC parallel loop async(1)

commit 170431fdc72df69e211ac5f741b9a4a4c75c904e

Fix a bug in ictherm=12
The bug was introduced during a refactoring in 4f675f6

src/init_temp.f90
@@ -185,7 +185,7 @@ subroutine init_geotherm_profile(n, i, age_1n, tp1n, tp2n)
 
             ! ss part with radiogenic heat
             if (y <= ymoho) then
-                tss = t_top + qm/cond*y + (dens_c*hs*hr*hr*1.d+6/cond)*(1d0-exp(-exp(-y/hr)))
+                tss = t_top + qm/cond*y + (dens_c*hs*hr*hr*1.d+6/cond)*(1d0-exp(-y/hr)))
             elseif (y <= yL0) then ! below moho, inside lithosphere
                 tss = tm + qm/cond*(y-ymoho)
             else